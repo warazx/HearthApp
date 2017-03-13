e27fdfc92d0adba0b9507eef57f68cb02d471679
Christian Karlsson <krille.karlsson@gmail.com>
2017-03-13 16:38:44 +0100

    changed to buttons, added segue

--- HearthApp/DeckClassPickerViewController.swift
+++ HearthApp/DeckClassPickerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  BuildDeckViewController.swift
+//  DeckClassPickerViewController.swift
 //  HearthApp
 //
 //  Created by Christian Karlsson on 2017-03-13.
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class BuildDeckViewController: UIViewController {
+class DeckClassPickerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func classPicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "classpicker", sender: sender)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +25,17 @@ class BuildDeckViewController: UIViewController {
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "classpicker", let destination = segue.destination as? DeckBuilderViewController {
+            let button = sender as! UIButton
+            NSLog("test")
+            destination.pickedClass = button.currentTitle!
+        }
     }
-    */
 
 }
